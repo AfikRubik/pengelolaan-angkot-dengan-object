5f570b1088305c7dd2175cda12bbda057c666609
AfikRubik <dzulfikar.af@gmail.com>
2022-12-12 18:45:42 +0700

    Add tambahPenumpang()

--- angkot.js
+++ angkot.js
@@ -6,4 +6,11 @@ function Angkot(namaSupir, trayek, penumpang, kas) {
 };
 
 var angkot1 = new Angkot('Joko', ['Tembalang', 'Banyumanik'], [], 0);
-var angkot2 = new Angkot('Boni', ['Gayamsai', 'Kaligawe'], [], 0);
+var angkot2 = new Angkot('Boni', ['Gayamsari', 'Kaligawe'], [], 0);
+
+function tambahPenumpang(namaPenumpang, nomorAngkot) {
+  if (nomorAngkot.penumpang.length === 0) {
+    nomorAngkot.penumpang.push(namaPenumpang);
+    return nomorAngkot.penumpang;
+  }
+}
